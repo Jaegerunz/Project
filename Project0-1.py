0f0b24ae978699100f530e18dde04a77f8d05c26
Jagermind <jagerunz@gmail.com>
2024-10-25 17:03:10 -0400

    for loop works. Final for 0.1

--- Project0-1.py
+++ Project0-1.py
@@ -1,6 +1,7 @@
 #This is the start of Project 0-1
 #AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
 import sys
+
 allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
 menuSelect= 0
 #print statements for input request
@@ -10,12 +11,15 @@ print("********************************")
 #input request
 menuSelect=int(input("Please make a selection from the following menu: ""\n " "\n1. PRINT all Authorized Vehicles" "\n2.Exit""\n" ))
 #selection logic
+#i had to google some For syntax but this is much neater, i'm leaving the old prints statements in comment as a reference
 if menuSelect == 1:
-    print("test")
-    print(allowedVehiclesList[0])
-    print(allowedVehiclesList[1])
-    print(allowedVehiclesList[2])
-    print(allowedVehiclesList[3])
-    print(allowedVehiclesList[4])
+    for car in allowedVehiclesList:
+        print(car)
+    #print(allowedVehiclesList[0])
+    #print(allowedVehiclesList[1])
+    #print(allowedVehiclesList[2])
+    #print(allowedVehiclesList[3])
+    #print(allowedVehiclesList[4])
 if menuSelect ==2:
+    print("Thank you for using AutoCountry Vehicle Finder, good-bye!")
     sys.exit()
