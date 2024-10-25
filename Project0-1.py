ece39c90c61f29f64e7f0d55ea35348de3338153
Jagermind <jagerunz@gmail.com>
2024-10-25 16:29:32 -0400

    Initial commit

---
+++ Project0-1.py
@@ -0,0 +1,4 @@
+#This is the start of Project 0-1
+#AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
+
+allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
