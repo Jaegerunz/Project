2be5ec22ad81da4d2338d8efa1bc20a5a190212c
Jagermind <jagerunz@gmail.com>
2024-10-25 16:47:58 -0400

    WORKS. but change print > for loop?

--- Project0-1.py
+++ Project0-1.py
@@ -1,14 +1,21 @@
 #This is the start of Project 0-1
 #AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
-
+import sys
 allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
+menuSelect= 0
 #print statements for input request
 print("********************************")
 print("AutoCountry Vehicle Finder v0.1")
 print("********************************")
-print("Please make a selection from the following menu: ""\n " "\n1. PRINT all Authorized Vehicles" "\n2.Exit" )
 #input request
-
-#menu option 1
-
-#menu option 2
+menuSelect=int(input("Please make a selection from the following menu: ""\n " "\n1. PRINT all Authorized Vehicles" "\n2.Exit""\n" ))
+#selection logic
+if menuSelect == 1:
+    print("test")
+    print(allowedVehiclesList[0])
+    print(allowedVehiclesList[1])
+    print(allowedVehiclesList[2])
+    print(allowedVehiclesList[3])
+    print(allowedVehiclesList[4])
+if menuSelect ==2:
+    sys.exit()
