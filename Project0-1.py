069aeb133fc20769fa0bef9f35759418d91e7fc4
Jagermind <jagerunz@gmail.com>
2024-10-25 16:35:28 -0400

    menu functions

--- Project0-1.py
+++ Project0-1.py
@@ -2,3 +2,13 @@
 #AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
 
 allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Trundra', 'Nissan Titan']
+#print statements for input request
+print("********************************")
+print("AutoCountry Vehicle Finder v0.1")
+print("********************************")
+print("Please make a selection from the following menu: ""\n " "\n1. PRINT all Authorized Vehicles" "\n2.Exit" )
+#input request
+
+#menu option 1
+
+#menu option 2
